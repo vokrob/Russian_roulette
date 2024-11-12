6605f4375ed457ce4857d01728005c6b522f6da3
vokrob <106639488+vokrob@users.noreply.github.com>
2024-11-12 14:28:56 +0700

    Changing minSdkVersion from 24 to 21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "com.vokrob.russian_roulette"
-        minSdk = 24
+        minSdk = 21
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
